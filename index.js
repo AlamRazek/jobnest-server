bffe3de10e778c452a24735ef48c8550c7e542bc
AlamRazek <ohidul933gmail.com>
2023-11-09 00:40:57 +0000

    json web token is created

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xqlvbzz.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,7 +36,16 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       console.log(user);
-      res.send(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send(token);
     });
 
     // Send a ping to confirm a successful connection
